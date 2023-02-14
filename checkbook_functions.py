9df16fc4c7b09e68f85628b66da329b50597949d
Josh Burch <joshuaburch01@gmail.com>
2023-02-14 10:29:39 -0600

    checkbook functions mvp working

--- checkbook_functions.py
+++ checkbook_functions.py
@@ -1,3 +1,6 @@
+import csv
+import time
+import os
 
 def view():
     balance = get_balance()
@@ -7,7 +10,7 @@ def view():
 def withdraw(num):
     balance = float(get_balance())
     if num > balance:
-        print("sorry you to broke for that!")
+        print("Sorry you to broke for that!")
     else:
         balance -= num
         str(change_balance(balance))
@@ -20,10 +23,24 @@ def deposit(num):
 
 
 def get_balance():
-    with open("checkbook.txt", 'r') as f:
-        return f.read()
+    if os.path.exists('checkbook.txt'):
+        with open('checkbook.txt', 'r') as f:
+            return f.read()
+    else:
+        with open('checkbook.txt', 'w') as f:
+            f.write('0.00')
+        with open('checkbook.txt', 'r') as f:
+            return f.read()
+
 
 
 def change_balance(amount):
     with open("checkbook.txt", 'w') as f:
         f.write(str(amount))
+
+
+def check_input(a):
+    return a.isdigit() or a.replace(".", "").isdigit()
+
+
+
